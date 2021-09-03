252917cd7dafeceb197ad9af56c1cd5fbde083a3
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-04 05:49:52 +0800

    :white_check_mark: Add unit test for token issuer

--- test/itmantokencrowdsale.test.js
+++ test/itmantokencrowdsale.test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("ITManTokenCrowdsale", () => {
-  it("Should have 70% of ITManToken", async () => {
+  it("Should have 70% of ITManToken tokens", async () => {
     const ITManToken = await ethers.getContractFactory("ITManToken");
     const itManToken = await ITManToken.deploy();
     await itManToken.deployed();
@@ -25,6 +25,19 @@ describe("ITManTokenCrowdsale", () => {
       totalSupply.mul(ethers.BigNumber.from(70)).div(ethers.BigNumber.from(100))
     );
 
+    expect(await itManTokenCrowdsale.rate()).to.equal(rate);
     expect(await itManTokenCrowdsale.remainingTokens()).to.equal(ethers.BigNumber.from("700000000000000000000000"));
+
+    // send 500 wei to the crowdsale contract address
+    const signer = await ethers.getSigner(1);
+    await signer.sendTransaction({
+      to: itManTokenCrowdsale.address,
+      value: ethers.BigNumber.from(rate),
+    });
+
+    // check that the remaining tokens last than 70% of the total supply
+    expect(await itManTokenCrowdsale.weiRaised()).to.equal(ethers.BigNumber.from(rate));
+    expect(await itManTokenCrowdsale.remainingTokens()).to.equal(ethers.BigNumber.from("699999999999999999750000"));
+    expect(await itManToken.balanceOf(signer.address)).to.equal(250000);
   });
 });
