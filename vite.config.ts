6613f5d410d53cc86e328cd5749b5e91e9a33a97
Huynh Duc Dung <dunghd.it@gmail.com>
2022-08-27 19:10:35 +0800

    :arrow_up: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), checker({ typescript: true })],
+  plugins: [reactRefresh()],
 });
